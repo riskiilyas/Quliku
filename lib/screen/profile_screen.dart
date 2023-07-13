8c21d504fa4435a5c3da7ae1f239d6df92febb7a
riskiilyas <riskiilyas03@gmail.com>
2023-07-13 21:36:03 +0700

    finish upper profile

--- lib/screen/profile_screen.dart
+++ lib/screen/profile_screen.dart
@@ -43,175 +43,101 @@ class _MyHomePageState extends State<ProfileScreen> {
                       child: Row(
                         crossAxisAlignment: CrossAxisAlignment.start,
                         children: [
-                          SizedBox(
-                            width: 80,
-                            child: Image.asset(
-                              "assets/dummy-profile.png",
-                              fit: BoxFit.fill,
+                          Expanded(
+                            flex: 1,
+                            child: SizedBox(
+                              width: 80,
+                              child: Image.asset(
+                                "assets/dummy-profile.png",
+                                fit: BoxFit.fill,
+                              ),
                             ),
                           ),
-                          SizedBox(width: 16,),
-                          Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Text(
-                                "Husin Muhammad Assegaff Arrabbanii",
-                                maxLines: 2,
-                                overflow: TextOverflow.ellipsis,
-                                style: const TextStyle(
-                                    color: Constants.COLOR_TEXT,
-                                    fontSize: 20,
-                                    fontWeight: FontWeight.bold
+                          SizedBox(
+                            width: 16,
+                          ),
+                          Expanded(
+                            flex: 3,
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  "Husin Muhammad Assegaff Arrabbanii",
+                                  maxLines: 2,
+                                  overflow: TextOverflow.ellipsis,
+                                  style: const TextStyle(
+                                      color: Constants.COLOR_TITLE,
+                                      fontSize: 16,
+                                      fontWeight: FontWeight.bold),
                                 ),
-                              ),
-                            ],
+                                SizedBox(
+                                  height: 2,
+                                ),
+                                Row(
+                                  children: const [
+                                    Icon(
+                                      Icons.construction,
+                                      size: 12,
+                                    ),
+                                    Text(
+                                      "2 Proyek Berjalan",
+                                      style: TextStyle(
+                                        color: Constants.COLOR_TEXT,
+                                        fontSize: 12,
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                                SizedBox(
+                                  height: 2,
+                                ),
+                                Row(
+                                  children: [
+                                    Icon(
+                                      Icons.check,
+                                      size: 12,
+                                    ),
+                                    Text(
+                                      "2 Proyek Berjalan",
+                                      style: const TextStyle(
+                                        color: Constants.COLOR_TEXT,
+                                        fontSize: 12,
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                                SizedBox(height: 16,),
+                                Row(
+                                  mainAxisAlignment: MainAxisAlignment.end,
+                                  crossAxisAlignment: CrossAxisAlignment.end,
+                                  children: [
+                                    InkWell(
+                                      onTap: () => {},
+                                      child: const Text(
+                                        "Edit Profil",
+                                        style: TextStyle(
+                                          color: Constants.COLOR_MAIN,
+                                          fontWeight: FontWeight.bold,
+                                          fontSize: 14,
+                                        ),
+                                      ),
+                                    ),
+                                  ],
+                                )
+                              ],
+                            ),
                           )
                         ],
                       ),
                     ),
                   ),
-                  const Text(
-                    "Halo, riskiilyas@gmail.com",
-                    style: TextStyle(
-                        color: Constants.COLOR_TITLE,
-                        fontSize: 20,
-                        fontWeight: FontWeight.bold),
-                  ),
-                  const SizedBox(
-                    height: 8,
-                  ),
-                  const Text(
-                    "cari mandor yang sesuai dengan kebutuhan anda",
-                    style: TextStyle(
-                      color: Constants.COLOR_TEXT,
-                    ),
-                  ),
-                  const SizedBox(
-                    height: 16,
-                  ),
-                  InkWell(
-                    onTap: () => {},
-                    child: Container(
-                        padding: const EdgeInsets.all(16.0),
-                        decoration: BoxDecoration(
-                          borderRadius:
-                          const BorderRadius.all(Radius.circular(24)),
-                          border: Border.all(
-                              width: 4, color: Constants.COLOR_MAIN),
-                        ),
-                        child: Row(
-                          children: const [
-                            Icon(
-                              Icons.search,
-                              color: Constants.COLOR_MAIN,
-                            ),
-                            Text(
-                              "Cari Mandor",
-                              style: TextStyle(
-                                  color: Constants.COLOR_TITLE,
-                                  fontWeight: FontWeight.bold),
-                            )
-                          ],
-                        )),
-                  ),
-                  const SizedBox(
-                    height: 24,
-                  ),
-                  const Text(
-                    "Klasifikasi Mandor",
-                    style: TextStyle(
-                        color: Constants.COLOR_TITLE,
-                        fontSize: 16,
-                        fontWeight: FontWeight.bold),
-                  ),
-                  const SizedBox(
-                    height: 4,
-                  ),
-                  const Text(
-                    "pilih mandor yang sesuai dengan kebutuhan anda",
-                    style: TextStyle(
-                      color: Constants.COLOR_TEXT,
-                    ),
-                  ),
-                  const SizedBox(
-                    height: 8,
-                  ),
                   Card(
-                    elevation: 2,
-                    child: Padding(
-                      padding: const EdgeInsets.symmetric(vertical: 16.0),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                        children: [
-                          KlasifikasiButton(
-                              text: "Sumber Daya Air",
-                              imgUrl: "assets/homepage_air.png",
-                              onPressed: () => {}),
-                          KlasifikasiButton(
-                              text: "Infrastruktur",
-                              imgUrl: "assets/homepage_infra.png",
-                              onPressed: () => {}),
-                          KlasifikasiButton(
-                              text: "Pencipta Karyaan",
-                              imgUrl: "assets/homepage_karya.png",
-                              onPressed: () => {})
-                        ],
-                      ),
+                    elevation: 1,
+                    child: Column(
+                      children: [
+
+                      ],
                     ),
-                  ),
-                  const SizedBox(
-                    height: 12,
-                  ),
-                  const Text(
-                    "Rekomendasi Mandor",
-                    style: TextStyle(
-                        color: Constants.COLOR_TITLE,
-                        fontSize: 16,
-                        fontWeight: FontWeight.bold),
-                  ),
-                  const SizedBox(
-                    height: 4,
-                  ),
-                  const Text(
-                    "Pilihan mandor terbaik yang dapat anda pilih",
-                    style: TextStyle(
-                      color: Constants.COLOR_TEXT,
-                    ),
-                  ),
-                  MandorItem(
-                      fullname: "Muhammad Faris Akbar",
-                      rating: 4.5,
-                      experience: 12,
-                      rangeKuli: "40 - 70 kuli",
-                      location: "Sidoarjo",
-                      imgUrl: "assets/dummy-profile.png",
-                      onPressed: () => {}),
-                  MandorItem(
-                      fullname: "Muhammad Faris Akbar",
-                      rating: 4.5,
-                      experience: 12,
-                      rangeKuli: "40 - 70 kuli",
-                      location: "Sidoarjo",
-                      imgUrl: "assets/dummy-profile.png",
-                      onPressed: () => {}),
-                  MandorItem(
-                      fullname: "Muhammad Faris Akbar",
-                      rating: 4.5,
-                      experience: 12,
-                      rangeKuli: "40 - 70 kuli",
-                      location: "Sidoarjo",
-                      imgUrl: "assets/dummy-profile.png",
-                      onPressed: () => {}),
-                  MandorItem(
-                      fullname: "Muhammad Faris Akbar",
-                      rating: 4.5,
-                      experience: 12,
-                      rangeKuli: "40 - 70 kuli",
-                      location: "Sidoarjo",
-                      imgUrl: "assets/dummy-profile.png",
-                      onPressed: () => {}),
-                  const SizedBox(
-                    height: 24,
                   ),
                 ],
               ),
