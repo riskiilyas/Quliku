ec1aef9b9c8eda963b077d147b7696a128e6b8e6
riskiilyas <riskiilyas03@gmail.com>
2023-07-13 21:17:12 +0700

    fix overflow on mandor item

--- lib/screen/profile_screen.dart
+++ lib/screen/profile_screen.dart
@@ -22,170 +22,201 @@ class _MyHomePageState extends State<ProfileScreen> {
         backgroundColor: Colors.white,
         centerTitle: false,
         title: const Text(
-          "Profile",
+          "Profil Saya",
           style: TextStyle(
               color: Constants.COLOR_TITLE, fontWeight: FontWeight.bold),
         ),
       ),
       body: SafeArea(
-        child: Container(
-          padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 0),
-          child: Column(
-            children: [
-              Expanded(
-                child: ListView(
-                  children: [
-                    const SizedBox(
-                      height: 24,
-                    ),
-                    const Text(
-                      "Halo, riskiilyas@gmail.com",
-                      style: TextStyle(
-                          color: Constants.COLOR_TITLE,
-                          fontSize: 20,
-                          fontWeight: FontWeight.bold),
-                    ),
-                    const SizedBox(
-                      height: 8,
-                    ),
-                    const Text(
-                      "cari mandor yang sesuai dengan kebutuhan anda",
-                      style: TextStyle(
-                        color: Constants.COLOR_TEXT,
-                      ),
-                    ),
-                    const SizedBox(
-                      height: 16,
-                    ),
-                    InkWell(
-                      onTap: () => {},
-                      child: Container(
-                          padding: const EdgeInsets.all(16.0),
-                          decoration: BoxDecoration(
-                            borderRadius:
-                            const BorderRadius.all(Radius.circular(24)),
-                            border: Border.all(
-                                width: 4, color: Constants.COLOR_MAIN),
+        child: Column(
+          children: [
+            Expanded(
+              child: ListView(
+                children: [
+                  const SizedBox(
+                    height: 24,
+                  ),
+                  Card(
+                    elevation: 1,
+                    child: Padding(
+                      padding: const EdgeInsets.all(16.0),
+                      child: Row(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          SizedBox(
+                            width: 80,
+                            child: Image.asset(
+                              "assets/dummy-profile.png",
+                              fit: BoxFit.fill,
+                            ),
                           ),
-                          child: Row(
-                            children: const [
-                              Icon(
-                                Icons.search,
-                                color: Constants.COLOR_MAIN,
-                              ),
+                          SizedBox(width: 16,),
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
                               Text(
-                                "Cari Mandor",
-                                style: TextStyle(
-                                    color: Constants.COLOR_TITLE,
-                                    fontWeight: FontWeight.bold),
-                              )
+                                "Husin Muhammad Assegaff Arrabbanii",
+                                maxLines: 2,
+                                overflow: TextOverflow.ellipsis,
+                                style: const TextStyle(
+                                    color: Constants.COLOR_TEXT,
+                                    fontSize: 20,
+                                    fontWeight: FontWeight.bold
+                                ),
+                              ),
                             ],
-                          )),
-                    ),
-                    const SizedBox(
-                      height: 24,
-                    ),
-                    const Text(
-                      "Klasifikasi Mandor",
-                      style: TextStyle(
-                          color: Constants.COLOR_TITLE,
-                          fontSize: 16,
-                          fontWeight: FontWeight.bold),
-                    ),
-                    const SizedBox(
-                      height: 4,
-                    ),
-                    const Text(
-                      "pilih mandor yang sesuai dengan kebutuhan anda",
-                      style: TextStyle(
-                        color: Constants.COLOR_TEXT,
+                          )
+                        ],
                       ),
                     ),
-                    const SizedBox(
-                      height: 8,
+                  ),
+                  const Text(
+                    "Halo, riskiilyas@gmail.com",
+                    style: TextStyle(
+                        color: Constants.COLOR_TITLE,
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold),
+                  ),
+                  const SizedBox(
+                    height: 8,
+                  ),
+                  const Text(
+                    "cari mandor yang sesuai dengan kebutuhan anda",
+                    style: TextStyle(
+                      color: Constants.COLOR_TEXT,
                     ),
-                    Card(
-                      elevation: 2,
-                      child: Padding(
-                        padding: const EdgeInsets.symmetric(vertical: 16.0),
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                          children: [
-                            KlasifikasiButton(
-                                text: "Sumber Daya Air",
-                                imgUrl: "assets/homepage_air.png",
-                                onPressed: () => {}),
-                            KlasifikasiButton(
-                                text: "Infrastruktur",
-                                imgUrl: "assets/homepage_infra.png",
-                                onPressed: () => {}),
-                            KlasifikasiButton(
-                                text: "Pencipta Karyaan",
-                                imgUrl: "assets/homepage_karya.png",
-                                onPressed: () => {})
-                          ],
+                  ),
+                  const SizedBox(
+                    height: 16,
+                  ),
+                  InkWell(
+                    onTap: () => {},
+                    child: Container(
+                        padding: const EdgeInsets.all(16.0),
+                        decoration: BoxDecoration(
+                          borderRadius:
+                          const BorderRadius.all(Radius.circular(24)),
+                          border: Border.all(
+                              width: 4, color: Constants.COLOR_MAIN),
                         ),
+                        child: Row(
+                          children: const [
+                            Icon(
+                              Icons.search,
+                              color: Constants.COLOR_MAIN,
+                            ),
+                            Text(
+                              "Cari Mandor",
+                              style: TextStyle(
+                                  color: Constants.COLOR_TITLE,
+                                  fontWeight: FontWeight.bold),
+                            )
+                          ],
+                        )),
+                  ),
+                  const SizedBox(
+                    height: 24,
+                  ),
+                  const Text(
+                    "Klasifikasi Mandor",
+                    style: TextStyle(
+                        color: Constants.COLOR_TITLE,
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold),
+                  ),
+                  const SizedBox(
+                    height: 4,
+                  ),
+                  const Text(
+                    "pilih mandor yang sesuai dengan kebutuhan anda",
+                    style: TextStyle(
+                      color: Constants.COLOR_TEXT,
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 8,
+                  ),
+                  Card(
+                    elevation: 2,
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 16.0),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          KlasifikasiButton(
+                              text: "Sumber Daya Air",
+                              imgUrl: "assets/homepage_air.png",
+                              onPressed: () => {}),
+                          KlasifikasiButton(
+                              text: "Infrastruktur",
+                              imgUrl: "assets/homepage_infra.png",
+                              onPressed: () => {}),
+                          KlasifikasiButton(
+                              text: "Pencipta Karyaan",
+                              imgUrl: "assets/homepage_karya.png",
+                              onPressed: () => {})
+                        ],
                       ),
                     ),
-                    const SizedBox(
-                      height: 12,
+                  ),
+                  const SizedBox(
+                    height: 12,
+                  ),
+                  const Text(
+                    "Rekomendasi Mandor",
+                    style: TextStyle(
+                        color: Constants.COLOR_TITLE,
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold),
+                  ),
+                  const SizedBox(
+                    height: 4,
+                  ),
+                  const Text(
+                    "Pilihan mandor terbaik yang dapat anda pilih",
+                    style: TextStyle(
+                      color: Constants.COLOR_TEXT,
                     ),
-                    const Text(
-                      "Rekomendasi Mandor",
-                      style: TextStyle(
-                          color: Constants.COLOR_TITLE,
-                          fontSize: 16,
-                          fontWeight: FontWeight.bold),
-                    ),
-                    const SizedBox(
-                      height: 4,
-                    ),
-                    const Text(
-                      "Pilihan mandor terbaik yang dapat anda pilih",
-                      style: TextStyle(
-                        color: Constants.COLOR_TEXT,
-                      ),
-                    ),
-                    MandorItem(
-                        fullname: "Muhammad Faris Akbar",
-                        rating: 4.5,
-                        experience: 12,
-                        rangeKuli: "40 - 70 kuli",
-                        location: "Sidoarjo",
-                        imgUrl: "assets/dummy-profile.png",
-                        onPressed: () => {}),
-                    MandorItem(
-                        fullname: "Muhammad Faris Akbar",
-                        rating: 4.5,
-                        experience: 12,
-                        rangeKuli: "40 - 70 kuli",
-                        location: "Sidoarjo",
-                        imgUrl: "assets/dummy-profile.png",
-                        onPressed: () => {}),
-                    MandorItem(
-                        fullname: "Muhammad Faris Akbar",
-                        rating: 4.5,
-                        experience: 12,
-                        rangeKuli: "40 - 70 kuli",
-                        location: "Sidoarjo",
-                        imgUrl: "assets/dummy-profile.png",
-                        onPressed: () => {}),
-                    MandorItem(
-                        fullname: "Muhammad Faris Akbar",
-                        rating: 4.5,
-                        experience: 12,
-                        rangeKuli: "40 - 70 kuli",
-                        location: "Sidoarjo",
-                        imgUrl: "assets/dummy-profile.png",
-                        onPressed: () => {}),
-                    const SizedBox(
-                      height: 24,
-                    ),
-                  ],
-                ),
-              )
-            ],
-          ),
+                  ),
+                  MandorItem(
+                      fullname: "Muhammad Faris Akbar",
+                      rating: 4.5,
+                      experience: 12,
+                      rangeKuli: "40 - 70 kuli",
+                      location: "Sidoarjo",
+                      imgUrl: "assets/dummy-profile.png",
+                      onPressed: () => {}),
+                  MandorItem(
+                      fullname: "Muhammad Faris Akbar",
+                      rating: 4.5,
+                      experience: 12,
+                      rangeKuli: "40 - 70 kuli",
+                      location: "Sidoarjo",
+                      imgUrl: "assets/dummy-profile.png",
+                      onPressed: () => {}),
+                  MandorItem(
+                      fullname: "Muhammad Faris Akbar",
+                      rating: 4.5,
+                      experience: 12,
+                      rangeKuli: "40 - 70 kuli",
+                      location: "Sidoarjo",
+                      imgUrl: "assets/dummy-profile.png",
+                      onPressed: () => {}),
+                  MandorItem(
+                      fullname: "Muhammad Faris Akbar",
+                      rating: 4.5,
+                      experience: 12,
+                      rangeKuli: "40 - 70 kuli",
+                      location: "Sidoarjo",
+                      imgUrl: "assets/dummy-profile.png",
+                      onPressed: () => {}),
+                  const SizedBox(
+                    height: 24,
+                  ),
+                ],
+              ),
+            )
+          ],
         ),
       ),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
