b15c5d8a019194069963bf2667a3e7d12d3f8c88
riskiilyas <riskiilyas03@gmail.com>
2023-07-13 18:24:01 +0700

    finish klasifikasi mandor homepage

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -1,6 +1,7 @@
 import 'package:curved_navigation_bar/curved_navigation_bar.dart';
 import 'package:flutter/material.dart';
 import 'package:quliku/util/Constants.dart';
+import 'package:quliku/widget/custom_klasifikasi_button.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -57,24 +58,95 @@ class _MyHomePageState extends State<HomeScreen> {
                     const SizedBox(
                       height: 16,
                     ),
-                    Container(
-                        padding: const EdgeInsets.all(16.0),
-                        decoration: BoxDecoration(
-                          borderRadius: const BorderRadius.all(Radius.circular(24)),
-                          border: Border.all(width: 4, color: Constants.COLOR_MAIN),
-                        ),
-                        child: Row(
-                          children: const [
-                            Icon(Icons.search, color: Constants.COLOR_MAIN,),
-                            Text(
-                              "Cari Mandor",
-                              style: TextStyle(
-                                color: Constants.COLOR_TITLE,
-                                fontWeight: FontWeight.bold
+                    InkWell(
+                      onTap: () => {},
+                      child: Container(
+                          padding: const EdgeInsets.all(16.0),
+                          decoration: BoxDecoration(
+                            borderRadius:
+                                const BorderRadius.all(Radius.circular(24)),
+                            border: Border.all(
+                                width: 4, color: Constants.COLOR_MAIN),
+                          ),
+                          child: Row(
+                            children: const [
+                              Icon(
+                                Icons.search,
+                                color: Constants.COLOR_MAIN,
                               ),
-                            )
+                              Text(
+                                "Cari Mandor",
+                                style: TextStyle(
+                                    color: Constants.COLOR_TITLE,
+                                    fontWeight: FontWeight.bold),
+                              )
+                            ],
+                          )),
+                    ),
+                    const SizedBox(
+                      height: 24,
+                    ),
+                    const Text(
+                      "Klasifikasi Mandor",
+                      style: TextStyle(
+                          color: Constants.COLOR_TITLE,
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold),
+                    ),
+                    const SizedBox(
+                      height: 4,
+                    ),
+                    const Text(
+                      "pilih mandor yang sesuai dengan kebutuhan anda",
+                      style: TextStyle(
+                        color: Constants.COLOR_TEXT,
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 8,
+                    ),
+                    Card(
+                      elevation: 2,
+                      child: Padding(
+                        padding: const EdgeInsets.symmetric(vertical: 16.0),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: [
+                            KlasifikasiButton(
+                                text: "Sumber Daya Air",
+                                imgUrl: "assets/homepage_air.png",
+                                onPressed: () => {}),
+                            KlasifikasiButton(
+                                text: "Infrastruktur",
+                                imgUrl: "assets/homepage_infra.png",
+                                onPressed: () => {}),
+                            KlasifikasiButton(
+                                text: "Pencipta Karyaan",
+                                imgUrl: "assets/homepage_karya.png",
+                                onPressed: () => {})
                           ],
-                        ))
+                        ),
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 12,
+                    ),
+                    const Text(
+                      "Rekomendasi Mandor",
+                      style: TextStyle(
+                          color: Constants.COLOR_TITLE,
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold),
+                    ),
+                    const SizedBox(
+                      height: 4,
+                    ),
+                    const Text(
+                      "Pilihan mandor terbaik yang dapat anda pilih",
+                      style: TextStyle(
+                        color: Constants.COLOR_TEXT,
+                      ),
+                    ),
                   ],
                 ),
               )
---
+++ lib/widget/custom_mandor_item.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:quliku/util/Constants.dart';
+
+class KlasifikasiButton extends StatelessWidget {
+  final String text;
+  final String imgUrl;
+  final Function() onPressed;
+
+  const KlasifikasiButton(
+      {Key? key,
+        required this.text,
+        required this.imgUrl,
+        required this.onPressed})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: onPressed,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Center(
+            child: SizedBox(
+              width: 56,
+              child: Image.asset(
+                imgUrl,
+                fit: BoxFit.fill,
+              ),
+            ),
+          ),
+          const SizedBox(height: 4,),
+          Text(
+            text,
+            style: const TextStyle(
+                color: Constants.COLOR_TEXT,
+                fontWeight: FontWeight.bold,
+                fontSize: 12
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
