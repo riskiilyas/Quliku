c189f28fcffb5e168af33e0eabfcac0fb0690a0a
riskiilyas <riskiilyas03@gmail.com>
2023-07-13 17:50:03 +0700

    finish top side homepage

--- lib/util/Constants.dart
+++ lib/util/Constants.dart
@@ -1,8 +1,8 @@
 import 'dart:ui';
 
 class Constants {
-  static const COLOR_MAIN = const Color(0xFFF88214);
-  static const COLOR_HINT_TEXT = const Color(0xFFC2C2C2);
-  static const COLOR_TITLE = const Color(0xFF333333);
-  static const COLOR_TEXT = const Color(0xFF4D4D4D);
+  static const COLOR_MAIN = Color(0xFFF88214);
+  static const COLOR_HINT_TEXT = Color(0xFFC2C2C2);
+  static const COLOR_TITLE = Color(0xFF505050);
+  static const COLOR_TEXT = Color(0xFF707070);
 }
