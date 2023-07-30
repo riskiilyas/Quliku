a67d1e75105471824d8167304ed210fe9cdb13b7
riskiilyas <riskiilyas03@gmail.com>
2023-07-30 11:51:36 +0700

    fix: update register ui

--- lib/screen/register_screen.dart
+++ lib/screen/register_screen.dart
@@ -43,158 +43,131 @@ class _MyHomePageState extends State<RegisterScreen> {
     init();
     return Scaffold(
       body: SafeArea(
-          child: Container(
-        padding: const EdgeInsets.symmetric(vertical: 24, horizontal: 16),
-        child: Column(
-          children: [
-            Expanded(
-              child: ListView(
-                children: [
-                  Expanded(
-                    flex: 1,
-                    child: SizedBox(
-                      width: double.infinity,
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: const [
-                          Text(
-                            'Daftar',
+          child: ListView(
+        children: [
+          Expanded(
+            child: Column(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 24, horizontal: 16),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      const Text(
+                        'Daftar',
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 24,
+                            color: Constants.COLOR_MAIN),
+                      ),
+                      const Divider(
+                        color: Constants.COLOR_MAIN,
+                      ),
+                      const SizedBox(
+                        height: 4,
+                      ),
+                      const Text(
+                        'Yuk segera daftarkan dirimu!',
+                        style: TextStyle(color: Constants.COLOR_TEXT),
+                      ),
+                      const SizedBox(
+                        height: 16,
+                      ),
+                      CustomTextField(
+                        hint: "Nama Lengkap",
+                        icon: Icons.abc,
+                        callback: (_) => fullname = _,
+                      ),
+                      const SizedBox(
+                        height: 8,
+                      ),
+                      CustomTextField(
+                        hint: "Username",
+                        icon: Icons.person,
+                        callback: (_) => username = _,
+                      ),
+                      const SizedBox(
+                        height: 8,
+                      ),
+                      CustomTextField(
+                        hint: "Email",
+                        icon: Icons.email,
+                        callback: (_) => email = _,
+                      ),
+                      const SizedBox(
+                        height: 8,
+                      ),
+                      CustomTextField(
+                        hint: "Password",
+                        icon: Icons.password,
+                        callback: (_) => password = _,
+                      ),
+                      const SizedBox(
+                        height: 8,
+                      ),
+                      CustomTextField(
+                        hint: "Confirm Password",
+                        icon: Icons.password,
+                        callback: (_) => confirmPassword = _,
+                      ),
+                      const SizedBox(
+                        height: 24,
+                      ),
+                      status != FetchStatus.LOADING && status != FetchStatus.SUCCESS
+                          ? CustomButton(
+                          text: "DAFTAR",
+                          textColor: Colors.white,
+                          buttonColor: Constants.COLOR_MAIN,
+                          onPressed: () {
+                            context.read<RegisterNotifier>().fetch(fullname,
+                                username, email, password, confirmPassword);
+                          })
+                          : const SpinKitFadingCircle(
+                        color: Constants.COLOR_MAIN,
+                        size: 50.0,
+                      ),
+                      const SizedBox(
+                        height: 12,
+                      ),
+                      Row(
+                        children: [
+                          const Text(
+                            'Sudah Punya Akun? ',
                             style: TextStyle(
-                                fontWeight: FontWeight.bold,
-                                fontSize: 24,
-                                color: Constants.COLOR_TITLE),
+                                color: Constants.COLOR_HINT_TEXT, fontSize: 16),
                           ),
-                          SizedBox(
-                            height: 8,
+                          InkWell(
+                            onTap: () => {
+                              // Constants.goto(LoginScreen(blocContext: widget.blocContext))
+                            },
+                            child: const Text(
+                              'Masuk',
+                              style: TextStyle(
+                                  color: Constants.COLOR_MAIN,
+                                  fontSize: 16,
+                                  fontWeight: FontWeight.bold),
+                            ),
                           ),
-                          Text(
-                            'Yuk segera daftarkan dirimu!',
-                            style: TextStyle(color: Constants.COLOR_TEXT),
-                          )
                         ],
                       ),
-                    ),
+                    ],
                   ),
-                  const SizedBox(
-                    height: 16,
+                ),
+                const SizedBox(
+                  height: 16,
+                ),
+                Container(
+                  alignment: Alignment.bottomRight,
+                  child: Image.asset(
+                    "assets/bg_quliku_icon.png",
+                    width: 256,
+                    alignment: Alignment.bottomRight,
                   ),
-                  Expanded(
-                    flex: 3,
-                    child: Center(
-                      child: SizedBox(
-                        width: 160,
-                        child: Image.asset(
-                          'assets/register_img.png',
-                          fit: BoxFit.fill,
-                        ),
-                      ),
-                    ),
-                  ),
-                  const SizedBox(
-                    height: 16,
-                  ),
-                  Expanded(
-                    flex: 5,
-                    child: Column(
-                      children: [
-                        CustomTextField(
-                          hint: "Nama Lengkap",
-                          icon: Icons.abc,
-                          callback: (_) => fullname = _,
-                        ),
-                        const SizedBox(
-                          height: 8,
-                        ),
-                        CustomTextField(
-                          hint: "Username",
-                          icon: Icons.person,
-                          callback: (_) => username = _,
-                        ),
-                        const SizedBox(
-                          height: 8,
-                        ),
-                        CustomTextField(
-                          hint: "Email",
-                          icon: Icons.email,
-                          callback: (_) => email = _,
-                        ),
-                        const SizedBox(
-                          height: 8,
-                        ),
-                        CustomTextField(
-                          hint: "Password",
-                          icon: Icons.password,
-                          callback: (_) => password = _,
-                        ),
-                        const SizedBox(
-                          height: 8,
-                        ),
-                        CustomTextField(
-                          hint: "Confirm Password",
-                          icon: Icons.password,
-                          callback: (_) => confirmPassword = _,
-                        )
-                      ],
-                    ),
-                  ),
-                  const SizedBox(
-                    height: 24,
-                  ),
-                  Expanded(
-                    flex: 1,
-                    child: Column(
-                      children: [
-                        status != FetchStatus.LOADING &&
-                                status != FetchStatus.SUCCESS
-                            ? CustomButton(
-                                text: "DAFTAR",
-                                textColor: Colors.white,
-                                buttonColor: Constants.COLOR_MAIN,
-                                onPressed: () {
-                                  context.read<RegisterNotifier>().fetch(
-                                      fullname,
-                                      username,
-                                      email,
-                                      password,
-                                      confirmPassword);
-                                })
-                            : const SpinKitFadingCircle(
-                                color: Constants.COLOR_MAIN,
-                                size: 50.0,
-                              ),
-                        const SizedBox(
-                          height: 12,
-                        ),
-                        Row(
-                          children: [
-                            const Text(
-                              'Sudah Punya Akun? ',
-                              style: TextStyle(
-                                  color: Constants.COLOR_HINT_TEXT,
-                                  fontSize: 16),
-                            ),
-                            InkWell(
-                              onTap: () => {
-                                // Constants.goto(LoginScreen(blocContext: widget.blocContext))
-                              },
-                              child: const Text(
-                                'Masuk',
-                                style: TextStyle(
-                                    color: Constants.COLOR_MAIN,
-                                    fontSize: 16,
-                                    fontWeight: FontWeight.bold),
-                              ),
-                            )
-                          ],
-                        )
-                      ],
-                    ),
-                  ),
-                ],
-              ),
-            )
-          ],
-        ),
+                )
+              ],
+            ),
+          ),
+        ],
       )),
     );
   }
